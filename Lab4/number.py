e3e58e8db0aa16ad73f1bf2ce1ebd85df10a71e4
Guest <guest-UsidX5@meet.mit.edu>
2014-02-05 19:57:14 +0200

    Lab4

---
+++ Lab4/number.py
@@ -0,0 +1,22 @@
+class Integer(object):
+	def __init__(self, n, posneg):
+		self.n = n	
+		self.posneg = posneg	
+	def display(self):
+		print self.posneg + str(self.n)
+
+class NegatineInteger(Integer):
+	def __init__(self, n)
+		super(NegatineInteger, self)
+		self.NegativeInteger = negint
+	def negativeinteger(self):
+		return "-" + self.n
+
+
+#if __name__ == "__main__":
+	#test = Integer(2, "-")	
+	#test.display()
+ 
+
+
+	
